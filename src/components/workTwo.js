f6c512b89b5fcabe8c404d3e2f0150e1fef00065
Tevaryan Thiagarajan <tevaryan1993@gmail.com>
2019-08-16 00:36:30 +0800

    changed text

--- src/components/workTwo.js
+++ src/components/workTwo.js
@@ -16,7 +16,7 @@ class WorkTwo extends Component {
 
               <div style={{width:'100%', backgroundColor: '#E5E5E5', opacity:'0.95',position:'absolute', top:'200px', left:'250px', boxShadow: '2px 2px 5px grey', textAlign:'justify'}} className="workDescriptionContainer my-auto mx-auto text-start p-3 w-80">
                 <h2 style={{fontSize:"150%", color:'#696969'}} className="font-weight-bold">Flooring</h2>
-                <p className="workDescription" style={{fontSize:"100%"}}> They say the floor is the sole of the house, no home is complete without an amazing looking floor.From tile hacking and overlay to marble and parquet installation and polish. Elevate your home experience with our great flooring options.</p>
+                <p className="workDescription" style={{fontSize:"100%"}}> They say the floor is the sole of the house, no home is complete without an amazing looking floor. Have your current tiles hacked and overlayed, or replace them with marble or parquet flooring. Elevate your home experience with our great flooring options.</p>
               </div>
 
             </div>
